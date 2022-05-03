02fe0d8f236bd0eedf92df17c1f6a0f6082484e8
CorrosiveSt <valdis.kuusisto@protonmail.com>
2022-05-03 16:23:07 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('OTcwODk4MjcyNDg5MTc3MTc4.YnCpeg.rqqYNLWdrZSqGaJxBXI6Xq6aFa4')
